5c3193b9c71ae6829f1c10d318676aa49dea47cd
JmTiong <jmtiong92@gmail.com>
2016-02-16 13:42:44 +0800

    Implemented the skeleton of the Logic file
    - Added the overload methods, arguments and default return type

--- src/dotdotdot/Logic.java
+++ src/dotdotdot/Logic.java
@@ -1,29 +1,51 @@
 package dotdotdot;
 
+import java.util.ArrayList;
+
 public class Logic {
 
 	public Logic() {
 		// Do case based on pass in command
 	}
 	
-	public void addTask() {
-		
+	public boolean addTask(String task) {
+		return false;
 	}
 	
-	public void editTask() {
-		
+	public boolean addTask(String task, ArrayList<String> categories){
+		return false;
 	}
 	
-	public void deleteTask() {
-    	
+	public boolean addTask(String task, String preposition, String date){
+		return false;
+	}
+	
+	public boolean addTask(String task, String preposition, String date, ArrayList<String> categories){
+		return false;
+	}
+	
+	public boolean editTask() {
+		return false;
+	}
+	
+	public boolean deleteTask(int taskID) {
+		return false;
+    }
+	
+	public boolean deleteTask(ArrayList<Integer> taskIDs){
+		return false;
+	}
+    
+	public boolean syncList() {
+		return false;
     }
     
-	public void syncList() {
-    	
-    }
-    
-	public void writeToFile() {
-		
+	public boolean writeToFile() {
+		return false;
+	}
+	
+	public boolean doTask(int taskID){
+		return false;
 	}
     
 }
