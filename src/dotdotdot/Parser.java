e1dafa70afd623f4d5a1fc14ca26a12f56489124
Sam Tan <sylnnnn@gmail.com>
2016-02-17 17:00:35 +0800

    updated parser

--- src/dotdotdot/Parser.java
+++ src/dotdotdot/Parser.java
@@ -3,39 +3,48 @@ package dotdotdot;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import TextBuddy.COMMAND_TYPE;
+
 public class Parser {
+	public final int FIRST_ELEMENT = 0;
+	public final int SECOND_ELEMENT = 1;
+	public final int AFTER_PREPOSITION = 3;
+	public final int INVALID_ID = -1;
+	public final int SINGLE_DIGIT_DAY = 4;
+	
+	enum COMMAND_TYPE {
+		ADD, EDIT, DO, DELETE, INVALID
+	};
 
 	public Parser() {
 		
 	}
 	
 	/*
-	 * @param: String
-	 * returns boolean
+	 * @param String
+	 * @return boolean
 	 * 
 	 * true if user command is executed successfully
 	 * false otherwise
 	 */
 	public boolean input(String rawInput) {
-		String commandType = getCommand(rawInput);	
+		String commandTypeString = getCommand(rawInput);	
+
+		COMMAND_TYPE commandType = determineCommandType(commandTypeString);
 		
 		switch(commandType) {
-			// TODO: ADD TASK
-			case "add":
-				// if (1.0), i.e. no preposition AND no @category
+			case ADD:
 				addTask(rawInput);
-			break;
-			// TODO: DO/DELETE TASK
-			case "do":
+				break;
+			case DO:
 				doTask(rawInput);
-			break;
-			// TODO: EDIT TASK
-			case "edit":
-			
-			break;
+				break;
+			case EDIT:
+				editTask(rawInput);
+				break;
 			default:
-			// TODO: DOESN'T FIT INTO ANY OF THE ABOVE!!!
-			return false;
+				// TODO: DOESN'T FIT INTO ANY OF THE ABOVE!!!
+				return false;
 		}
 		
 		return true;
@@ -74,12 +83,45 @@ public class Parser {
 		
 		Logic logic = new Logic();
 		logic.doTask(taskID);
+		return true;
+	}
+	
+	private boolean editTask(String rawInput) {
+		ArrayList<String> inputParts = breakString(rawInput);
+		int taskID = convertToInt(getTaskID(inputParts));
+		String date = getDateFromRaw(inputParts);
+		
+		if(isInvalidID(taskID)) {
+			return false;
+		}
+		
+		Logic logic = new Logic();
+		logic.editTask(taskID, date);
 		
 		return true;
 	}
 	
+	private String getDateFromRaw(ArrayList<String> inputParts) {
+		String date = "";
+		
+		for(int i = AFTER_PREPOSITION; i < inputParts.size(); i++) {
+			date += inputParts.get(i);
+		}
+		
+		return formatDate(date);
+	}
+
+	private String formatDate(String date) {
+		String temp = date.replaceAll("\\s+","");
+		return temp.length() == SINGLE_DIGIT_DAY ? "0" + temp : temp;
+	}
+
+	private String getTaskID(ArrayList<String> inputParts) {
+		return inputParts.get(SECOND_ELEMENT);
+	}
+
 	private int convertToInt(String taskName) {
-		int taskID = -1;
+		int taskID = INVALID_ID;
 		try {
 			taskID = Integer.parseInt(taskName);
 		} catch (NumberFormatException nfe) {
@@ -88,21 +130,22 @@ public class Parser {
 	}
 
 	private boolean isInvalidID(int taskID) {
-		return taskID == -1 ? true : false;
+		return (taskID == INVALID_ID);
 	}
 
-	/*
-	 * returns the first token of a string, delimited by spaces
+	/**
+	 * @param rawInput
+	 * @return first token of rawInput, delimited by spaces
 	 */
 	private String getCommand(String rawInput) {
-		return rawInput.split(" ",1)[0];
+		return rawInput.split(" ",1)[FIRST_ELEMENT];
     }
 	
 	/*
 	 * returns string without first token, delimited by spaces
 	 */
 	private String getTaskName(String rawInput) {
-		return rawInput.split(" ",2)[1];
+		return rawInput.split(" ",2)[SECOND_ELEMENT];
     }
 	
 	/*
@@ -136,6 +179,23 @@ public class Parser {
 		}
 
         return hasFound;
+	}
+	
+	private static COMMAND_TYPE determineCommandType(String commandTypeString) {
+		if (commandTypeString == null)
+			throw new Error("command type string cannot be null!");
+
+		if (commandTypeString.equalsIgnoreCase("add")) {
+			return COMMAND_TYPE.ADD;
+		} else if (commandTypeString.equalsIgnoreCase("do")) {
+			return COMMAND_TYPE.DO;
+		} else if (commandTypeString.equalsIgnoreCase("delete")) {
+			return COMMAND_TYPE.DELETE;
+		} else if (commandTypeString.equalsIgnoreCase("edit")) {
+			return COMMAND_TYPE.EDIT;
+		} else {
+			return COMMAND_TYPE.INVALID;
+		}
 	}
 	
 }
