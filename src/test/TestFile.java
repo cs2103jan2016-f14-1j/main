b0791f097b58e2d69e02c1f8986bf04c9ce70bc7
JxJx <justjx@hotmail.com>
2016-03-23 15:30:44 +0800

    Add JUnit TestFile in folder/src/test

---
+++ src/test/TestFile.java
@@ -0,0 +1,26 @@
+package test;
+
+import logic.*;
+import parser.*;
+import java.util.Date;
+import java.util.ArrayList;
+import shared.*;
+import storage.Storage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestFile {
+	Parser parser = Parser.getInstance();
+	Logic logic = Logic.getInstance();
+	Storage storage = Storage.getInstance();
+	@Test
+	public void test() {
+		
+		// This is a test case for testing if the add function will add the following Strings
+		assertEquals(null, parser.parse("add addadd add"));
+		
+	}
+
+}
